306d9d9ed3e0ebfe571faf26ad7c9ab88ad81bb2
nkgw41 <nkgw41@gmail.com>
2012-11-13 20:49:13 +0900

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrefinery::Application.config.secret_token = 'eeb400fcbb7fda3ca432d72a9799866cf81b3c70ec32e8d5791181c70bdd0535a92e65a8e333126cef95ace6831cd8b3caf45e9f97ffaa58d0a969bb1229cfb5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Myrefinery::Application.config.session_store :cookie_store, key: '_myrefinery_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Myrefinery::Application.config.session_store :active_record_store
